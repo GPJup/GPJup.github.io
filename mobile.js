fbcd834103ca51c92656e21cc92d33b81c34efd4
GPJup <kozachukartem33@gmail.com>
2026-08-09 16:15:48 +0300

    Update mobile.js

--- mobile.js
+++ mobile.js
@@ -6,20 +6,27 @@ let index = 0;
 function setActive(i){
 
   cards.forEach((card, idx) => {
+
+    // Передаём каждой карточке её цвет
+    card.style.setProperty(
+      '--link-color',
+      card.dataset.color
+    );
+
     card.classList.toggle('active', idx === i);
   });
 
+  // Цвет аватарки = цвет активной карточки
   const color =
     cards[i].dataset.color || '#8b5cf6';
 
-  // Цвет аватарки
   root.style.setProperty('--accent', color);
 }
 
 // Первая активная
 setActive(0);
 
-// Плавный переход каждые 2.4 секунды
+// Плавный переход
 setInterval(() => {
   index = (index + 1) % cards.length;
   setActive(index);
